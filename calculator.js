04caf60ae6784d8e9dcec91afac80e7009b259bf
benhidayat <98013492+Benhidayat@users.noreply.github.com>
2024-05-23 20:24:10 +0700

    added some attributes to some elements that are gonna be used exclusively on js.
    added new js file and created variables for the
    calculator.

---
+++ calculator.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[delete-button]');
+const equalsButton = document.querySelector('[data-equals]');
+const previousOperandTextElement = document.querySelector('data-previous');
+const currentOperandTextElement = document.querySelector('data-current');
